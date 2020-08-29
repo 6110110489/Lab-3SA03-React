d8b0c8fa9047c9d6fccfc6c8c4e6280b306fe11d
6110110489 <6110110489@psu.ac.th>
2020-08-29 18:36:45 +0700

    part 7/2

--- src/WordCard.js
+++ src/WordCard.js
@@ -2,9 +2,14 @@ import React, {useState} from 'react';
 import CharacterCard from './CharacterCard';
 import _ from 'lodash';
 
+var correct = 0;
+var row = 0;
+var ans = "";
+
 const prepareStateFromWord = (given_word) => {
     let word = given_word.toUpperCase()
     let chars = _.shuffle(Array.from(word))
+
     return {
         word,
         chars,
@@ -26,10 +31,20 @@ export default function WordCard(props){
 
         if(guess.length == state.word.length){
             if(guess == state.word){
-                console.log('yeah!')
+                correct++;
+                console.log('yeah!' + correct)
+                alert('WOW ! You Correct ' + correct)
                 setState({...state, guess: '', completed: true})
+                if(correct == 4) {
+                    console.log("YOU WIN!")
+                    alertToWin();
+                    window.location.reload(false)
+                }
+            
             }else{
                 console.log('reset, next attempt')
+                alert("TRY AGAIN")
+                row++;
                 setState({...state, guess: '', attempt: state.attempt + 1})
             }
         } 
@@ -45,4 +60,23 @@ export default function WordCard(props){
             }
         </div>
     )
+}
+
+function alertToWin() {
+    var ale = Math.floor(Math.random() * 3);
+    if(ale == 0) {
+        alert("YOU WIN!");
+    }
+    else if(ale == 1) {
+        alert("YOUR ENGLISH SO GOOD!");
+    }
+    else{
+        alert("YOU SO WISE!");
+    }
+}
+
+function displayAnswer(word, rows) {
+    if(rows >= 3){
+      document.getElementById("demo").innerText = "Answer this question : "+ word;
+    }
 }
